fd13d53d179dc8a2ed9b6283922cb7a390c31df0
SolidNoob <anthony.cammarata@hotmail.com>
2021-06-27 16:50:55 +0200

    adding core fonctionnalities

--- monster_slayer/app.js
+++ monster_slayer/app.js
@@ -8,18 +8,50 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
+            currentRound: 0,
+            winner: null,
         }
     },
     computed: {
         monsterBarStyle() {
+            if(this.monsterHealth < 0) 
+                return { width: '0%' }
             return { width: this.monsterHealth + '%' }
         },
         playerBarStyle() {
+            if(this.playerHealth < 0) 
+                return { width: '0%' }
             return { width: this.playerHealth + '%' }
+        },
+        specialAttackIsAvailable() {
+            return this.currentRound % 3 !== 0
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw'
+            } else if (value <= 0) {
+                this.winner = 'monster'
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw'
+            } else if (value <= 0) {
+                this.winner = 'player'
+            }
         }
     },
     methods: {
+        resetGame() {
+            this.playerHealth = 100
+            this.monsterHealth = 100
+            this.currentRound = 0
+            this.winner = null
+        },
         attackMonster() {
+            this.currentRound++
             const attackValue = getRandomNumberBetween(5, 12)
             this.monsterHealth -= attackValue
             this.attackPlayer()
@@ -27,6 +59,24 @@ const app = Vue.createApp({
         attackPlayer() {
             const attackValue = getRandomNumberBetween(8, 15)
             this.playerHealth -= attackValue
+        },
+        specialAttackMonster() {
+            this.currentRound++
+            const attackValue = getRandomNumberBetween(10, 25)
+            this.monsterHealth -= attackValue
+            this.attackPlayer()
+        },
+        healPlayer() {
+            this.currentRound++
+            const healValue = getRandomNumberBetween(8, 20)
+            if (this.playerHealth + healValue > 100)
+                this.playerHealth = 100
+            else 
+                this.playerHealth += healValue
+            this.attackPlayer()
+        },
+        surrender() {
+            this.winner = 'monster'
         }
     }
 })
