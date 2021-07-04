efcd9d7ba9b9c363c02ffe9e5ab41336f1aabfc4
SolidNoob <anthony.cammarata@hotmail.com>
2021-07-04 12:17:23 +0200

    component communication

--- vue-first-app/src/App.vue
+++ vue-first-app/src/App.vue
@@ -1,38 +1,47 @@
 <template>
-    <section>
-        <header>
-            <h1>My friends</h1>
-        </header>
-        <ul>
-            <friend-contact></friend-contact>
-            <friend-contact></friend-contact>
-        </ul>
-    </section>
+  <section>
+    <header>
+      <h1>My friends</h1>
+    </header>
+    <ul>
+      <friend-contact
+        name="Manuel Lorenz"
+        phone-number="0123 45678 90"
+        email-address="manuel@localhost.com"
+        is-favorite="1"
+      ></friend-contact>
+      <friend-contact
+        name="Julie Jones"
+        phone-number="0987 6543 21"
+        email-address="julie@localhost.com"
+        is-favorite="0"
+      ></friend-contact>
+    </ul>
+  </section>
 </template>
 
 <script>
-import FriendContact from './components/FriendContact.vue'
+import FriendContact from "./components/FriendContact.vue";
 
 export default {
   components: { FriendContact },
-    data() {
-        return {
-    
-        friends: [
-                {
-                    id: 'julie',
-                    name: 'Julie Jones',
-                    phone: '0987 6543 21',
-                    email: 'julie@localhost.com'
-                }
-            ]
-        }
-    },
-    methods: {},
-    computed: {}
-}
+  data() {
+    return {
+      friends: [
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "0987 6543 21",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+  methods: {},
+  computed: {},
+};
 </script>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
 </style>
