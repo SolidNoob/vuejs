2b0de8195f8b91a11db3d83cf01ab3610d76b387
SolidNoob <anthony.cammarata@hotmail.com>
2021-07-04 14:42:32 +0200

    component practice

---
+++ components-practice/src/components/ActiveUser.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>Username: {{ username }}</h2>
+        <h3>Age: {{ age }}</h3>
+    </li>
+</template>
+
+<script>
+export default {
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+        age: {
+            type: Number,
+            required: true
+        }
+    },
+    data() {
+        return {}
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ components-practice/src/components/UserData.vue
@@ -0,0 +1,39 @@
+<template>
+  <form @submit.prevent="addUser">
+    <div>
+      <label>Name:</label>
+      <input type="text" v-model="enteredName" />
+    </div>
+    <div>
+      <label>Age:</label>
+      <input type="number" v-model="enteredAge" />
+    </div>
+    <div>
+      <button>Add Username</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-user"],
+  data() {
+    return {
+      enteredName: "",
+      enteredAge: 0,
+    };
+  },
+  methods: {
+    addUser() {
+      if (this.enteredName && this.enteredAge > 0) {
+        this.$emit("add-user", this.enteredName, parseInt(this.enteredAge));
+        this.enteredName = ''
+        this.enteredAge = 0
+      }
+    },
+  },
+};
+</script>
+
+<style>
+</style>
