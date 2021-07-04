1b6c3994f04b56464fac0f714439a0e69cc250c3
SolidNoob <anthony.cammarata@hotmail.com>
2021-07-04 13:58:32 +0200

    add form and two ways communication

--- vue-first-app/src/App.vue
+++ vue-first-app/src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <h1>My friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
@@ -13,6 +14,7 @@
         :email-address="friend.email"
         :is-favorite="friend.isFavorite"
         @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteFriend"
       ></friend-contact>
     </ul>
   </section>
@@ -20,9 +22,10 @@
 
 <script>
 import FriendContact from "./components/FriendContact.vue";
+import NewFriend from "./components/NewFriend.vue";
 
 export default {
-  components: { FriendContact },
+  components: { FriendContact, NewFriend },
   data() {
     return {
       friends: [
@@ -45,9 +48,24 @@ export default {
   },
   methods: {
     toggleFavoriteStatus(friendId) {
-      const identifiedFriend = this.friends.find(friend => friend.id === friendId)
-      identifiedFriend.isFavorite = ! identifiedFriend.isFavorite
+      const identifiedFriend = this.friends.find(
+        (friend) => friend.id === friendId
+      );
+      identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
     },
+    addContact(contactName, contactPhone, contactEmail) {
+      const newContact = {
+        id: new Date().toISOString(),
+        name: contactName,
+        phone: contactPhone,
+        email: contactEmail,
+        isFavorite: false,
+      }
+      this.friends.push(newContact)
+    },
+    deleteFriend(friendId) {
+      this.friends = this.friends.filter(friend => friendId !== friend.id)
+    }
   },
   computed: {},
 };
@@ -55,4 +73,83 @@ export default {
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Jost&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li,
+#app form {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
 </style>
