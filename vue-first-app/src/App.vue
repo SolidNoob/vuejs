7c7b13eb77690af15a362582b2dae6975b34c62b
SolidNoob <anthony.cammarata@hotmail.com>
2021-07-04 11:46:33 +0200

    CLI setup

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <header>
+            <h1>My friends</h1>
+        </header>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue'
+
+export default {
+  components: { FriendContact },
+    data() {
+        return {
+    
+        friends: [
+                {
+                    id: 'julie',
+                    name: 'Julie Jones',
+                    phone: '0987 6543 21',
+                    email: 'julie@localhost.com'
+                }
+            ]
+        }
+    },
+    methods: {},
+    computed: {}
+}
+</script>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
